8d542841ba6646f472a308cdbcefbe1744885126
bhim subedi <lovelotusist@hotmail.com>
2013-11-05 20:08:04 -0700

    login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+#class UsersController < ApplicationController
+#  def new
+#  end
+#end
+
+
+
+class UsersController < ApplicationController
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(params[:user])
+  if @user.save
+    redirect_to home_path, :notice => "Signed up!"
+  else
+    render "new"
+  end
+end
+end 
