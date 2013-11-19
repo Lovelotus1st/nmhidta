624e62cd7cd07d88bb4119bb9e75ceb8ad6e12cd
bhim subedi <lovelotusist@hotmail.com>
2013-11-19 01:13:06 -0700

    form update

---
+++ db/migrate/20131119041056_first_change_1.rb
@@ -0,0 +1,14 @@
+class FirstChange1 < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+  
+def change
+    change_column :databases, :time, :time
+    change_column :databases, :dob1, :date
+    change_column :databases, :dob2, :date
+  end
+  
+end
